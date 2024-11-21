f82abcad66df4c0e4524cb59810c575bd7b9b3a3
Rashy-hub <yagoubi.r@gmail.com>
2024-11-21 13:56:11 +0100

    feat : protected route component for the auth context

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,7 +1,6 @@
 import { Navigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 import PropTypes from 'prop-types'
-import { useEffect } from 'react'
 
 const ProtectedRoute = ({ element }) => {
     const { isAuthenticated, authUser } = useAuth()
