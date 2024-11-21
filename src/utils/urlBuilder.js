ce98d4f408523c9d430c7372ceada331b3de8dd1
Rashy-hub <yagoubi.r@gmail.com>
2024-11-21 13:57:26 +0100

    feat : url builder utils

---
+++ src/utils/urlBuilder.js
@@ -0,0 +1,21 @@
+const urlBuilder = ({ baseURL, endpoint, params, queries }) => {
+    const fullURL = `${baseURL.replace(/\/$/, '')}/${endpoint.replace(/^\//, '')}`
+    let url = new URL(fullURL)
+
+    // let endUrl = resolve(url, endpoint)
+    if (params && typeof params === 'object') {
+        Object.keys(params).forEach((key) => {
+            url.pathname = url.pathname.replace(`:${key}`, params[key])
+        })
+    }
+
+    if (queries && typeof queries === 'object') {
+        Object.keys(queries).forEach((key) => {
+            url.searchParams.append(key, queries[key])
+        })
+    }
+
+    return url.toString()
+}
+
+export default urlBuilder
