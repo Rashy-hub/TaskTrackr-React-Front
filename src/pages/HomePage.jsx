e8f914858c8c431f71b8d3e3126d7e32c9290d2a
Rashy-hub <yagoubi.r@gmail.com>
2024-11-24 19:05:56 +0100

    feat : fake progress bar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import HeaderHome from '../components/home/HeaderHome'
 import Hero from '../components/home/Hero'
 import { useTheme } from '../context/ThemeContext'
@@ -5,13 +6,34 @@ import HomeMain from '../layouts/HomeMain'
 
 const HomePage = () => {
     const { darkMode } = useTheme()
+    const [progress, setProgress] = useState(0)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setProgress((prev) => {
+                if (prev >= 100) {
+                    clearInterval(interval)
+                    return 100
+                }
+                return prev + 25
+            })
+        }, 100)
+        return () => clearInterval(interval)
+    }, [])
 
     return (
         <div className={`body-bg`} data-theme={darkMode ? 'dark' : 'light'}>
             <HeaderHome />
 
             <HomeMain>
-                {' '}
+                <div className="relative w-full h-2 bg-gray-200 dark:bg-gray-700 rounded">
+                    <div
+                        className={`w-[1px] absolute top-0 left-0 h-full transition-all duration-200 ease-in-out ${
+                            darkMode ? 'bg-[#4cb04f]' : 'bg-blue-500'
+                        }`}
+                        style={{ width: `${progress}%` }}
+                    />
+                </div>
                 <Hero />
             </HomeMain>
         </div>
