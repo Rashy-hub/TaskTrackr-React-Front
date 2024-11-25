bf8a6018fc3080ef084bacea1d22bc8162fec1f8
Rashy-hub <yagoubi.r@gmail.com>
2024-11-25 14:30:43 +0100

    fix : flex row reverse is not working

--- src/components/home/Features.jsx
+++ src/components/home/Features.jsx
@@ -4,32 +4,35 @@ const Features = () => {
     const [features, setFeatures] = useState([])
 
     useEffect(() => {
-        // Fetch the JSON file from the public folder
         fetch('/features.json')
             .then((response) => response.json())
             .then((data) => setFeatures(data))
             .catch((error) => console.error('Error loading JSON file:', error))
     }, [])
 
-    useEffect(() => {
-        console.log('Features changed:', JSON.stringify(features))
-    }, [features])
-
     return (
-        <section className="w-full">
+        <section className="w-full mt-4">
             {features.length > 0 ? (
                 features.map((feature, index) => (
-                    <div key={index} className="grid grid-cols-2 w-[80%] shadow-lg rounded-lg mt-12 mx-auto">
+                    <div
+                        key={index}
+                        className={`grid grid-cols-2 w-[80%] mt-12 mx-auto place-content-center place-items-center ${
+                            index % 2 === 1 ? 'flex-row-reverse' : ''
+                        }`}
+                    >
+                        {/* Text Section */}
                         <div className="flex flex-col items-center justify-start">
-                            <h2 className="text-4xl font-semibold mb-6">{feature.title}</h2>
+                            <h2 className="text-4xl font-semibold font-special mb-6">{feature.title}</h2>
                             <ul className="list-disc pl-6 mb-4">
                                 {feature.description.map((point, idx) => (
-                                    <li key={idx} className="text-4xl font-special text-neutral-100">
+                                    <li key={idx} className="text-2xl font-special text-neutral-100">
                                         {point}
                                     </li>
                                 ))}
                             </ul>
                         </div>
+
+                        {/* Image Section */}
                         <div className="w-[300px] h-[300px] place-self-center">
                             <img src={feature.image} alt={feature.title} className="w-full h-auto" />
                         </div>
